0db889a23acbd6e1a3eec15f6c78d218ff9cf461
Christopher Maier <cm@opscode.com>
2011-11-18 07:37:32 -0500

    Fix incorrect email address

--- test/sqerl_transformers_tests.erl
+++ test/sqerl_transformers_tests.erl
@@ -1,6 +1,6 @@
 %% -*- erlang-indent-level: 4;indent-tabs-mode: nil; fill-column: 92-*-
 %% ex: ts=4 sw=4 et
-%% @author Christopher Maier <maier@lambda.local>
+%% @author Christopher Maier <cm@opscode.com>
 %% @copyright 2011 Opscode, Inc.
 -module(sqerl_transformers_tests).
 
