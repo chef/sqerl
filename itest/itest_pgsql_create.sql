b8f45bca5a63c6c0754626dc72d18fc00d049533
Mark Anderson <ot-git@muking.org>
2011-11-18 18:54:25 -0800

    Fix permissions for user itest.

--- itest/itest_pgsql_create.sql
+++ itest/itest_pgsql_create.sql
@@ -1,11 +1,15 @@
 CREATE USER itest;
 CREATE DATABASE itest OWNER itest;
+GRANT ALL PRIVILEGES ON DATABASE itest TO itest;
 
 \c itest;
-CREATE SEQUENCE users_id_sequence
+CREATE SEQUENCE users_id_sequence;
 /* Create test tables */
 CREATE TABLE users (
        id int DEFAULT nextval('users_id_sequence') PRIMARY KEY,
        first_name varchar(80),
        last_name varchar(80)
 );
+
+GRANT ALL PRIVILEGES ON TABLE users TO itest;
+GRANT ALL PRIVILEGES ON SEQUENCE users_id_sequence TO itest;
