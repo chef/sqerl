282483fbe3ec401633069da95475abff6512f135
Mark Anderson <ot-git@muking.org>
2011-11-20 23:19:42 -0800

    Refactor makefile to isolate database specific config.

---
+++ mysql_conf.mk
@@ -0,0 +1,8 @@
+#
+# Config variables for make
+#
+DB_HOST = "localhost"
+DB_PORT = 3306
+DB_NAME = itest
+DB_TYPE = mysql
+DB_CMD = mysql -u root --host=${DB_HOST} --protocol=TCP
---
+++ pgsql_conf.mk
@@ -0,0 +1,9 @@
+#
+# config vars for make
+#
+DB_HOST = "localhost"
+DB_PORT = 5432
+DB_NAME = itest
+DB_TYPE = pgsql
+# using the postges database keeps us from having errors about droping current database
+DB_CMD = psql -d postgres -h ${DB_HOST} -p ${DB_PORT} -f -
