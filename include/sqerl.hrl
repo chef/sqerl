b7dfd8d35a4d1604a929329dca55833dbce504d0
Matthew Peck <matthew@opscode.com>
2012-07-12 09:52:18 -0400

    Added sqerl_error type

--- include/sqerl.hrl
+++ include/sqerl.hrl
@@ -1,3 +1,7 @@
 -define(ALL(Record), {rows_as_records, [Record, record_info(fields, Record)]}).
 
 -define(FIRST(Record), {first_as_record, [Record, record_info(fields, Record)]}).
+
+-type sqerl_error() :: {error, term()} |
+                        {conflict, term()} |
+                        {foreign_key, term()}.
