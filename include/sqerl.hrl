6c31fec7bea7938a1b47c3ed27e72ffd6cd6131c
Matthew Peck <matthew@opscode.com>
2012-07-23 20:17:16 -0400

    add comment explaining why sqerl_error type is vague

--- include/sqerl.hrl
+++ include/sqerl.hrl
@@ -2,4 +2,5 @@
 
 -define(FIRST(Record), {first_as_record, [Record, record_info(fields, Record)]}).
 
+%% This is intentionally vague since sqerl error codes are now user configurable
 -type sqerl_error() :: {atom(), term()}.
