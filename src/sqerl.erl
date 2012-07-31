d76e704ad026a9b506530c01f8c4efad05d525cf
doubt72 <doug@opscode.com>
2012-07-31 10:58:57 -0600

    Handle connection errors

--- src/sqerl.erl
+++ src/sqerl.erl
@@ -40,7 +40,7 @@ with_db(_Call, 0) ->
 with_db(Call, Retries) ->
     case pooler:take_member("sqerl") of
         error_no_members ->
-            {error, no_members};
+            {error, no_connections};
         error_no_pool ->
             {error, {no_pool, "sqerl"}};
         Cn when is_pid(Cn) ->
@@ -119,8 +119,16 @@ parse_error(Reason) ->
     {ok, DbType} = application:get_env(sqerl, db_type),
     parse_error(DbType, Reason).
 
--spec parse_error(mysql | pgsql, {term(), term()}
+-spec parse_error(mysql | pgsql, atom() | {term(), term()}
                         | {error, {error, error, _, _, _}}) -> sqerl_error().
+parse_error(_DbType, Error) when is_atom(Error) ->
+    case Error of
+        no_connection ->
+            {error, <<"No connection available or pool out of connections">>};
+        _ ->
+            {error, Error}
+    end;
+
 parse_error(mysql, Error) ->
     do_parse_error(Error, ?MYSQL_ERROR_CODES);
 
