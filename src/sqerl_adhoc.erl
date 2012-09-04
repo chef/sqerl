b816e1379480d261c8b8ef6f422423984ffe6f81
langloisjp <jpl@opscode.com>
2012-09-04 10:34:36 -0700

    Add adhoc insert and delete functionality.

--- src/sqerl_adhoc.erl
+++ src/sqerl_adhoc.erl
@@ -21,7 +21,9 @@
 
 -module(sqerl_adhoc).
 
--export([select/3]).
+-export([select/3,
+         delete/2,
+         insert/4]).
 
 -ifdef(TEST).
 -compile([export_all]).
@@ -78,12 +80,74 @@ select(Columns, Table, Where) ->
              <<"WHERE">>,
              where_parts(Where)],
     Query = join(Parts, <<" ">>),
+    list_to_binary(Query).
+
+
+%% @doc Generate DELETE statement
+%%
+%% See select/3 for details about the "Where" parameter.
+%%
+%% 1> delete(<<"users">>, {<<"id">>, equals, qmark}).
+%% <<"DELETE FROM users WHERE id = ?">>
+%%
+-spec delete(binary(), any()) -> binary().
+delete(Table, Where) ->
+    ensure_safe(Table),
+    Parts = [<<"DELETE FROM">>,
+             Table,
+             <<"WHERE">>,
+             where_parts(Where)],
+    Query = join(Parts, <<" ">>),
     list_to_binary(lists:flatten(Query)).
 
+
+%% @doc Generate INSERT statement.
+%%
+%% 1> insert(<<"users">>, [<<"id">>, <<"name">>], 3, qmark).
+%% <<"INSERT INTO users (name) VALUES (?,?),(?,?),(?,?)">>
+%%
+-spec insert(binary(), [binary()], integer(), atom()) -> binary().
+insert(Table, Columns, NumRows, ParamStyle) when
+    NumRows > 0,
+    length(Columns) > 0 ->
+    ensure_safe([Table, Columns]),
+    Parts = [<<"INSERT INTO">>,
+             Table,
+             <<"(">>,
+             column_parts(Columns),
+             <<")">>,
+             <<"VALUES">>,
+             values_parts(length(Columns), NumRows, ParamStyle)],
+    Query = join(Parts, <<" ">>),
+    list_to_binary(lists:flatten(Query)).
+
+%% @doc Generate values parts of insert query
+-spec values_parts(integer(), integer(), atom()) -> [binary()].
+values_parts(NumColumns, NumRows, ParamStyle) ->
+    values_parts(NumColumns, NumRows, ParamStyle, 0).
+
+%% @doc Generate values parts with parameter placeholders offset.
+%% Offset is useful for parameter placeholder styles that use numbers
+%% e.g. dollarn for e.g. postgresql.
+-spec values_parts(integer(), integer(), atom(), integer()) -> [binary()].
+values_parts(NumColumns, NumRows, ParamStyle, Offset) ->
+    ValuesPartF = fun(RowIndex) -> values_part(NumColumns, RowIndex * NumColumns + Offset, ParamStyle) end,
+    Parts = [ValuesPartF(RowIndex) || RowIndex <- lists:seq(0, NumRows-1)],
+    join(Parts, <<",">>).
+
+%% @doc Generates values part for one row
+-spec values_part(integer(), integer(), atom()) -> [binary()].
+values_part(NumColumns, Offset, ParamStyle) ->
+    [<<"(">>]
+    ++ join(placeholders(NumColumns, Offset, ParamStyle), <<",">>)
+    ++ [<<")">>].
+
 %% @doc Generate columns parts of query
+%% (Just joins them with comma).
 column_parts(Columns) -> join(Columns, <<",">>).
 
 %% @doc Generate "WHERE" parts of query.
+%% See select/3 for more details.
 where_parts({Field, equals, ParamStyle}) ->
     ensure_safe([Field]),
     [Field, <<" = ">>, placeholder(1, ParamStyle)];
@@ -131,9 +195,16 @@ ensure_safe([H|T]) ->
 %% 1> placeholders(3, dollarn).
 %% [<<"$1">>,<<"$2">>,<<"$3">>]
 %%
+%% The 3-argument version takes an offset argument.
+%%
+%% 1> placeholders(3, 4, dollarn).
+%% [<<"$5">>,<<"$6">>,<<"$7">>]
+%%
 -spec placeholders(integer(), atom()) -> [binary()].
-placeholders(NumValues, Style) when NumValues > 0 ->
-    [placeholder(N, Style) || N <- lists:seq(1, NumValues)].
+placeholders(NumValues, Style) ->
+    placeholders(NumValues, 0, Style).
+placeholders(NumValues, Offset, Style) when NumValues > 0 ->
+    [placeholder(N + Offset, Style) || N <- lists:seq(1, NumValues)].
 
 %% @doc Returns a parameter string for the given style
 %% at position N in the statement.
@@ -141,7 +212,7 @@ placeholders(NumValues, Style) when NumValues > 0 ->
 placeholder(_Pos, qmark) ->
     <<"?">>;
 placeholder(Pos, dollarn) ->
-    list_to_binary("$" ++ integer_to_list(Pos)).
+    list_to_binary([<<"$">>, integer_to_list(Pos)]).
 
 
 %%
@@ -153,8 +224,11 @@ placeholder(Pos, dollarn) ->
 %% 1> join([1,2,3], 0).
 %% [1,0,2,0,3]
 %%
-join([], _Sep) ->
-    [];
-join([H|T], Sep) ->
-    [H] ++ lists:append([[Sep] ++ [X] || X <- T]).
+%%join([], _Sep) -> [];
+%%join([H|T], Sep) -> [H] ++ lists:append([[Sep] ++ [X] || X <- T]).
 
+join([], _Sep) -> [];
+join(L, Sep) -> join(L, Sep, []).
+
+join([H], _Sep, Acc)  -> lists:reverse([H|Acc]);
+join([H|T], Sep, Acc) -> join(T, Sep, [Sep, H|Acc]).
