6e67d64cc96ecd053fd9ebef0bed802af0262df1
Oliver Ferrigni <oliver@opscode.com>
2013-10-17 13:21:21 -0500

    Correct sqerl when defaulting to allow default

--- src/sqerl_client.erl
+++ src/sqerl_client.erl
@@ -218,7 +218,7 @@ drivermod() ->
                                                  db_type,
                                                  "use db_driver_mod instead"}),
                     sqerl_pgsql_client;
-                BadType ->
+                BadType when BadType /= sqerl_pgsql_client->
                     log_and_error({unsupported_db_type, sqerl, BadType})
             end;
         DriverMod when is_atom(DriverMod) ->
