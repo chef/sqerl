d95a116509f944ad1e83b2630dd08e86b99f0578
Jean Rouge <jean@getchef.com>
2014-06-17 12:13:12 -0700

    Making it easier to prepare queries to update or delete on several fields.
    Will came back to that later to actually make it possible to call `sqerl_rec:delete(Rec, [Field1, Field2])`

--- test/sqerl_rec_tests.erl
+++ test/sqerl_rec_tests.erl
@@ -379,9 +379,11 @@ gen_fetch_multiple_test_() ->
       || {{Rec, By}, E} <- Tests ].
 
 gen_delete_test() ->
-    Expect = ["DELETE FROM ", "kitchens",
+    Expect1 = ["DELETE FROM ", "kitchens",
               " WHERE ", "id", " = $1"],
-    ?assertEqual(Expect, sqerl_rec:gen_delete(kitchen, id)).
+    ?assertEqual(Expect1, sqerl_rec:gen_delete(kitchen, id)),
+    Expect2 = Expect1 ++ [" AND ", "name", " = $2"],
+    ?assertEqual(Expect2, sqerl_rec:gen_delete(kitchen, [id, name])).
 
 gen_params_test_() ->
     Tests = [{1, "$1"},
@@ -395,12 +397,12 @@ gen_update_test() ->
               " SET ",
               "name = $1, auth_token = $2, ssh_pub_key = $3, "
               "first_name = $4, last_name = $5, email = $6",
-              " WHERE ", "id", " = ", "$7",
+              " WHERE ", "id", " = $7", " AND ", "email", " = $8",
               " RETURNING ",
               "id, kitchen_id, name, auth_token, auth_token_bday, "
               "ssh_pub_key, "
               "first_name, last_name, email"],
-    ?assertEqual(Expect, sqerl_rec:gen_update(cook, id)).
+    ?assertEqual(Expect, sqerl_rec:gen_update(cook, [id, email])).
 
 gen_insert_test() ->
     Expect = ["INSERT INTO ", "cookers", "(",
